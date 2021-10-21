193f595260764aa107e1ddc042a6f789e62a2b4c
Deric Parra <parradeko@gmail.com>
2021-10-21 17:40:53 -0300

    Estilizar fundos com imgs

--- src/components/Page/styles.ts
+++ src/components/Page/styles.ts
@@ -1,26 +1,47 @@
 import styled from 'styled-components'
+import background1 from './assets/amggt.jpg'
+import background2 from './assets/bmwm3.jpg'
+import background3 from './assets/lambohuracan.jpg'
+import background4 from './assets/RRvelar.jpg'
+import background5 from './assets/toyotasupra.jpg'
+import background6 from './assets/nissangtr.jpg'
+import background7 from './assets/audir8.jpg'
 
 export const Container = styled.div`
 .colored:nth-child(1) {
-background: #b5e48c;
+    background-image: url(${background1});
+    background-size: cover;
+    background-position: center;
 }
 .colored:nth-child(2) {
-background: #99d98c;
+    background-image: url(${background2});
+    background-size: cover;
+    background-position: center;
 }
 .colored:nth-child(3) {
-background: #76c893;
+    background-image: url(${background3});
+    background-size: cover;
+    background-position: center;
 }
 .colored:nth-child(4) {
-background: #52b69a;
+    background-image: url(${background4});
+    background-size: cover;
+    background-position: center;
 }
 .colored:nth-child(5) {
-background: #34a0a4;
+    background-image: url(${background5});
+    background-size: cover;
+    background-position: center;
 }
 .colored:nth-child(6) {
-background: #168aad;
+    background-image: url(${background6});
+    background-size: cover;
+    background-position: center;
 }
 .colored:nth-child(7) {
-background: #1a759f;
+    background-image: url(${background7});
+    background-size: cover;
+    background-position: center;
 }`
 
 
